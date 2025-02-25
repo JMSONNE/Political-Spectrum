32d8366607e38d685d0d23115285a039aa7327fa
JMSONNE <jordynmsonne@gmail.com>
2025-02-25 01:08:22 -0600

    created welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
+import { Home } from './Components/Home'
+import { Quiz } from './Components/Quiz';
 
 function App() {
 
+  const [start, setStart] = useState(false);
+
+
+  const handleStart = () => {
+    if (start === true) {
+      return (
+        <Quiz />
+      )
+    }
+  }
+
   return (
     <>
-
+      <div>
+        <Home handleStart={handleStart} />
+      </div>
     </>
   )
 }
