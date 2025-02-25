01dc9e9012ee7e676b536d6f3ce8a2114c843751
JMSONNE <jordynmsonne@gmail.com>
2025-02-25 01:50:52 -0600

    working on setting up handling users answers

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -4,9 +4,37 @@ import questions from '../utils/questions.json'
 
 export const Quiz = () => {
 
+    const question = questions[0].question
+
     const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [isDone, setIsDone] = useState(false);
+    const [typeCount, setTypeCount] = useState(new Map([
+        ["A", 0], ["E", 0],
+        ["L", 0], ["R", 0]
+    ]));
+
+    const handleQuestions = () => {
+        setCurrentQuestion((prevQuestion) => {
+            if (prevQuestion < questions.length - 1) {
+                return prevQuestion + 1;
+            } else {
+                setIsDone(true);
+                return prevQuestion;
+            }
+        });
+    };
+
+    const handleAnswers = (answer) => {
+        setTypeCount()
+    };
 
     return (
-        <div>Quiz</div>
+        <div>
+            <h1 style={{ textAlign: 'center' }}>
+                {question}
+            </h1>
+            <button onClick={() => handleAnswers()}>Agree</button>
+            <button onClick={() => handleAnswers()} >Disagree</button>
+        </div >
     )
 }
