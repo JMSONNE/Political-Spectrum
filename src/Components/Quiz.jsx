3b1780a26f2fb680a5e342564914b5577dacf019
JMSONNE <jordynmsonne@gmail.com>
2025-02-25 01:37:15 -0600

    added authoritarian/egalitarian questions

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState } from 'react';
+import questions from '../utils/questions.json'
 
 export const Quiz = () => {
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
     return (
         <div>Quiz</div>
     )
